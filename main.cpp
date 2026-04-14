6403441b9bd39a5d47ebfab2263b8feba29754ee
m.bersenev <no email provided>
2026-04-14 13:22:13 +0300

    translation file name fix

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
-        const QString baseName = "EANScannerEmuSrc_" + QLocale(locale).name();
+        const QString baseName = "EANScannerEmu_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
